990cde0a49694d717282598af18acfc1613976ab
xsiry <xsiries@gmail.com>
2017-05-27 14:22:36 +0800

    #更新意见反馈

--- static/feedback/src/module.js
+++ static/feedback/src/module.js
@@ -1,16 +1,31 @@
 define(function(require, exports, module) {
-  $.root_ = $('#loginFrom');
+  $.root_ = $('.feedback_main');
   module.exports = {
-
     init: function() {
       this._bindUI();
-      inputValidator();
     },
     _bindUI: function() {
-      $.root_.off('click', '.subimt_btn').on('click', '.subimt_btn', function() {
-        loginSubmit();
+      $.root_.off('change', '.upload_files').on('change', '.upload_files', function() {
+        imgChange();
       })
     },
   };
 
-  })
+  function imgChange() {
+    //获取点击的文本框
+    var file = $(".upload_files");
+    //存放图片的父级元素
+    var fileList = file.get(0).files;
+    //遍历获取到得图片文件
+    for (var i = 0; i < fileList.length; i++) {
+      var img_length = $('.z_photo').children('img').length;
+      var imgUrl = window.URL.createObjectURL(fileList[i]);
+      var imgObj = $('<div class="z_addImg"><img src=' + imgUrl + '></img></div>');
+      if ( img_length <= 5) {
+        img_length >= 5 ? $('.z_file').hide() : $('.z_file').show();
+        $('.z_photo').prepend(imgObj);
+      }
+    };
+  };
+
+})
